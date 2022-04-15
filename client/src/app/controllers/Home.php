cf9befd35b156a7fbedf8be099dc266584e23de2
Tuan Nguyen <tuannguyentn2504@gmail.com>
2022-04-15 22:02:24 +0700

    Client: Test API connection

--- client/src/app/controllers/Home.php
+++ client/src/app/controllers/Home.php
@@ -3,6 +3,21 @@ class Home extends Controller
 {
     public function index()
     {
+        $apiRes = [];
+
+        array_push($apiRes, ApiCaller::get(AGGREGATE_SERVICE_API_URL . '/demo'));
+        array_push($apiRes, ApiCaller::get(INTERNAL_SERVICE_API_URL . '/demo'));
+        array_push($apiRes, ApiCaller::get(ORDER_SERVICE_API_URL . '/demo'));
+        array_push($apiRes, ApiCaller::get(PAYMENT_SERVICE_API_URL . '/demo'));
+        array_push($apiRes, ApiCaller::get(PRODUCT_SERVICE_API_URL . '/demo'));
+        array_push($apiRes, ApiCaller::get(REVIEW_SERVICE_API_URL . '/demo'));
+        array_push($apiRes, ApiCaller::get(SHIPPING_SERVICE_API_URL . '/demo'));
+        array_push($apiRes, ApiCaller::get(SHOP_SERVICE_API_URL . '/demo'));
+        array_push($apiRes, ApiCaller::get(SUPPORT_SERVICE_API_URL . '/demo'));
+        array_push($apiRes, ApiCaller::get(USER_SERVICE_API_URL . '/demo'));
+
+        $this->data['viewContent']['data'] = $apiRes;
+
         $this->setBasicData('home/index', 'Trang chủ');
         $this->data['cssLinks'] = ['home.css'];
         $this->data['jsLinks'] = ['home.js', 'scroll-top.js'];
