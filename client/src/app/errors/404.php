d7659e1f3c3659a945885ce6aa1cc5dd94c3346b
Tuan Nguyen <tuannguyentn2504@gmail.com>
2022-04-18 22:06:24 +0700

    Client: Add  404 notfound page

--- client/src/app/errors/404.php
+++ client/src/app/errors/404.php
@@ -1,1 +1,35 @@
-<h1 style="color:red;text-align: center;">404 Not Found</h1>
+<head>
+    <link rel="icon" type="image/x-icon" href="/public/assets/images/favicon.png">
+    <link rel="stylesheet" href="/public/assets/css/404.css" />
+    <title>Không tìm thấy trang | MetaMarket</title>
+</head>
+
+<div class="container">
+    <h1 class="first-four">4</h1>
+    <div class="cog-wheel1">
+        <div class="cog1">
+            <div class="top"></div>
+            <div class="down"></div>
+            <div class="left-top"></div>
+            <div class="left-down"></div>
+            <div class="right-top"></div>
+            <div class="right-down"></div>
+            <div class="left"></div>
+            <div class="right"></div>
+        </div>
+    </div>
+    <div class="cog-wheel2">
+        <div class="cog2">
+            <div class="top"></div>
+            <div class="down"></div>
+            <div class="left-top"></div>
+            <div class="left-down"></div>
+            <div class="right-top"></div>
+            <div class="right-down"></div>
+            <div class="left"></div>
+            <div class="right"></div>
+        </div>
+    </div>
+    <h1 class="second-four">4</h1>
+</div>
+<p class="wrong-para">Xin lỗi ! Trang không tìm thấy hoặc đang xảy ra xử cố. <a href="/">Về Trang chủ</a></p>
