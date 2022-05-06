1ee0f265e64120a2cca8c8803cdb7325ed481cbb
Tuan Nguyen <tuannguyentn2504@gmail.com>
2022-05-06 15:42:34 +0700

    FE: Add Image Utils for comporess, crop image

---
+++ client/src/utils/Image.php
@@ -0,0 +1,50 @@
+<?php
+class ImageUtil
+{
+    public static function createImageFromSource($source)
+    {
+        // get image info
+        $imgInfo = getimagesize($source);
+        $mime = $imgInfo['mime'];
+
+        // create a new image from source
+        switch ($mime) {
+            case 'image/jpeg':
+                $image = imagecreatefromjpeg($source);
+                break;
+            case 'image/png':
+                $image = imagecreatefrompng($source);
+                break;
+            case 'image/gif':
+                $image = imagecreatefromgif($source);
+                break;
+            default:
+                $image = imagecreatefromjpeg($source);
+                break;
+        }
+
+        return $image;
+    }
+
+    public static function compressImage($source, $destination, $quantity = 80)
+    {
+        $image = self::createImageFromSource($source);
+        imagejpeg($image, $destination, $quantity);
+        return $destination;
+    }
+
+    public static function createThumbnail($source, $destination, $thumbWidth = 100)
+    {
+        $sourceImage = self::createImageFromSource($source);
+
+        $orgWidth = imagesx($sourceImage);
+        $orgHeight = imagesy($sourceImage);
+        $thumbHeight = floor($orgHeight * ($thumbWidth / $orgWidth));
+        $destImage = imagecreatetruecolor($thumbWidth, $thumbHeight);
+
+        imagecopyresampled($destImage, $sourceImage, 0, 0, 0, 0, $thumbWidth, $thumbHeight, $orgWidth, $orgHeight);
+        imagejpeg($destImage, $destination);
+        imagedestroy($sourceImage);
+        imagedestroy($destImage);
+    }
+}
