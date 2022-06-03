fc38aea1db29e4a65d37fd90540919e23a1cb443
Đinh Quang Thọ <57233818+18120580@users.noreply.github.com>
2022-06-03 20:11:34 +0700

    Add dashboar for adminpage (#6)

---
+++ client/src/app/views/admin/shipper-list.php
@@ -0,0 +1,73 @@
+<?php
+require_once _DIR_ROOT . '/utils/Format.php';
+require_once _DIR_ROOT . '/utils/Convert.php';
+require_once _DIR_ROOT . '/app/views/mixins/pagination.php';
+?>
+<div class="shipperinfor">
+  <div class="searchshipper">
+    <input type="text" id="keyworshipper" placeholder="Nhập tên shipper">
+    <button type="submit" class="searchButton" id='searchshipper'>
+      <i class="bi bi-search" id="search"></i>
+    </button>
+  </div>
+  <?php
+  echo "
+   <div class='container'>
+     <table class='table table-striped'>
+       <thead>
+         <tr class='header'>
+           <th>Mã shipper
+           </th>
+           <th>Tên tài khoả
+           </th>
+           <th>Địa chỉ
+           </th>
+           <th>Giấy phép lái xe
+           </th>
+           <th>CMND/CCCD
+           </th>
+           <th>Tình trạng
+         </th>
+         </tr>
+       </thead>
+       <tbody>";
+  $shippers = $shipperData->result->rows;
+  $total = $shipperData->result->count;
+  $page = $shipperData->page;
+  $pagesize = $shipperData->pagesize;
+  $totalpage = ceil($total / $pagesize);
+  foreach ($shippers as $item) {
+    if ($item->status == 1)
+      echo "
+    <tr>
+    <td>$item->shipperId</td>
+    <td>$item->username</td>
+    <td>$item->address</td>
+    <td>$item->driverLicense</td>
+    <td>$item->peopleId</td>
+    <td>Hoạt động</td>
+    </tr>
+    ";
+    else {
+      echo "
+    <tr>
+    <td>$item->shipperId</td>
+    <td>$item->username</td>
+    <td>$item->address</td>
+    <td>$item->driverLicense</td>
+    <td>$item->peopleId</td>
+    <td>Không hoạt động</td>
+    </tr>
+    ";
+    }
+  }
+  echo "        </tbody>
+  </table>
+</div>";
+  echo "<div class='col col-12'>";
+  renderPagination($totalpage, $page);
+  echo "</div>";
+  ?>
+</div>
+<?php require_once _DIR_ROOT . '/app/views/blocks/scroll-top.php'; ?>
+</div>
---
+++ client/src/app/views/blocks/admin/header.php
@@ -0,0 +1,20 @@
+<header class="container-fluid flex-center-between w-100">
+    <div class='vertical-center'>
+        <a href="/" class="vertical-center logo-group">
+            <?php
+            $staticUrl = STATIC_FILE_URL;
+            echo "<img src='$staticUrl/assets/images/logo.svg' alt='MM Logo' class='logo me-2' width='100%' height='100%'>";
+            ?>
+            <strong class="logo-name desk">Meta<span class="orange-color">Market</span></strong>
+            <strong class="logo-name mobile d-none">M<span class="orange-color">M</span></strong>
+        </a>
+        <div class="sub-logo ms-3">Kênh quản lý</div>
+    </div>
+    </div>
+    <div class='vertical-center'>
+        <i class='bi bi-grid-3x3-gap toggle-navbar-icon' id="toggleNavbar" title="Đóng/mở menu"></i>
+        <button class='btn btn-logout ms-4'>
+            <a href='/account/logout'>Đăng Xuất</a>
+        </button>
+    </div>
+</header>
