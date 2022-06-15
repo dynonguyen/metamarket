bf8ee7272da43ce9eb9a9f049c064f5045f99ea8
TranqBuong <trantrang0902paxxa@gmail.com>
2022-06-15 23:34:00 +0700

    Add updateUserInfo

--- client/src/app/views/account/info.php
+++ client/src/app/views/account/info.php
@@ -1,8 +1,9 @@
 <?php
+    require_once _DIR_ROOT.'/utils/Format.php';
     $fullname = $user->_get('fullname');
     $phone = $user->_get('phone');
     $gender = $user->_get('gender');
-    $dbo = substr(str_replace('/','-', $user->_get('dbo')), 0, 10);
+    $dbo = FormatUtil::ISOChangeTimeZone($user->_get('dbo'), 'Y-m-d');
 ?>
 
 <div class="container_info">
@@ -25,30 +26,37 @@
                 <div class="spe">Quản lý thông tin hồ sơ để bảo mật tài khoản</div>
             </div>
             <?php
-                echo "<form action='' method='post'><div class='cnt'>
+                if (!empty($formError)) {
+                    echo "<p id='formError' class='form-error mb-3 color-item'>$formError</p>";
+                } else {
+                    echo "<p id='formError' class='form-error mb-3 color-item d-none'></p>";
+                }
+            ?>
+            <?php
+                echo "<form id='form' method='POST' action='/account/postUpdateInfo'><div class='cnt'>
                         <div class='cnt fullname'>
-                        <label for='fname' class='lb'>Họ tên</label>
-                        <input type='text' name='fname' value='$fullname'/>
+                        <label for='name' class='lb'>Họ tên</label>
+                        <input id='name' type='text' name='name' value='$fullname' minlength=5 maxlength=50/>
                     </div>
                     <div class='cnt phone'>
-                        <label for='fphone' class='lb'>Số điện thoại</label>
-                        <input type='tel' name='fphone' value='$phone'/>
+                        <label for='phone' class='lb'>Số điện thoại</label>
+                        <input id='phone' type='tel' name='phone' value='$phone' minlength=10 maxlength=10/>
                     </div>
                     <div class='cnt gender'>
-                        <label for='fgender' class='lb'>Giới tính</label>";
+                        <label for='gender' class='lb'>Giới tính</label>";
                     if($gender == FEMALE) {
                         echo "
-                        <input class='gd' name='fgender' type='radio' value='Nam'/><span class='sp'>Nam</spa>
-                        <input class='gd' name='fgender' type='radio' value='Nữ' checked/><span class='sp'>Nữ</spa>";
+                        <input class='gd' name='gender' type='radio' value='Nam'/><span class='sp'>Nam</spa>
+                        <input class='gd' name='gender' type='radio' value='Nữ' checked/><span class='sp'>Nữ</spa>";
                     } else {
                         echo "
-                        <input class='gd' name='fgender' type='radio' value='Nam' checked/><span class='sp'>Nam</span>
-                        <input class='gd' name='fgender' type='radio' value='Nữ'/><span class='sp'>Nữ</spa>";
+                        <input class='gd' name='gender' type='radio' value='Nam' checked/><span class='sp'>Nam</span>
+                        <input class='gd' name='gender' type='radio' value='Nữ'/><span class='sp'>Nữ</spa>";
                     };
                     echo "</div>
                     <div class='cnt dbo'>
-                        <label for='fdbo' class='lb'>Ngày sinh</label>
-                        <input name='fdbo' type='date' value='$dbo'/>
+                        <label for='dbo' class='lb'>Ngày sinh</label>
+                        <input id= 'dbo' name='dbo' type='date' value='$dbo' min='1952-01-01' max='2012-12-31'/>
                     </div>
                     <div class='cnt submit'>
                         <input id='up' type='submit' value='Cập nhật'/>
--- client/src/public/assets/js/account/info.js
+++ client/src/public/assets/js/account/info.js
@@ -1,1 +1,20 @@
-jQuery(function () {});
+function showFormError(msg = "") {
+  $("#formError").html(msg).removeClass("d-none");
+}
+
+jQuery(function () {
+  $("#form").on("submit", function (e) {
+    e.preventDefault();
+
+    const name = $("#name").val()?.trim();
+    const phone = $("#phone").val()?.trim();
+    const gender = $("#gender");
+    const dbo = $("#dbo");
+
+    if (!name || !phone || !gender || !phone) {
+      return showFormError("Vui lòng nhập đủ các trường !");
+    }
+
+    this.submit();
+  });
+});
